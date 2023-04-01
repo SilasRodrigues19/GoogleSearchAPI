46b0ea9ae056a234053f3ce2f7ff756525c06a18
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-04-01 14:13:31 -0300

    refactor: disabled deprecated routes

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,17 +5,24 @@ import { Icon } from '@iconify/react';
 
 const links = [
   { url: '/search', text: 'All', icon: 'ph:folder-open-light' },
-  { url: '/news', text: 'News', icon: 'ph:newspaper-light' },
+  { url: '/news', text: 'News', icon: 'ph:newspaper-light', disabled: true },
   { url: '/images', text: 'Images', icon: 'clarity:image-line' },
-  { url: '/videos', text: 'Videos', icon: 'uil:video' },
+  { url: '/videos', text: 'Videos', icon: 'uil:video', disabled: true },
 ];
 
 export const Links = () => (
-  <div className="flex sm:justify-around justify-between items-center mt-4">
-    {links.map(({ url, text, icon }) => (
-      <NavLink to={url} activeClassName="text-blue-500 border-b-2 dark:text-blue-300 border-blue-500 pb-2" className="flex">
+  <div className='flex sm:justify-around justify-between items-center mt-4'>
+    {links.map(({ url, text, icon, disabled }, index) => (
+      <NavLink
+        key={index}
+        to={url}
+        activeClassName='text-blue-500 border-b-2 dark:text-blue-300 border-blue-500 pb-2'
+        className={`flex ${disabled ? 'disabled cursor-not-allowed' : ''}`}
+        title={disabled ? 'This link is disabled' : ''}
+        onClick={(e) => disabled && e.preventDefault()}
+      >
         {text}
-        <Icon icon={icon} className="ml-2 mt-1" />
+        <Icon icon={icon} className='ml-2 mt-1' />
       </NavLink>
     ))}
   </div>
