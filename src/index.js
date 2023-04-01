0aede033c9b8f98fffc6001e033a3f4db5f24e2e
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-04-01 14:11:46 -0300

    fix: change to createRoot for compatibility with React 18

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import { StateContextProvider } from './contexts/StateContextProvider';
 import './global.css';
 
-ReactDom.render(
+const rootElement = document.getElementById('root');
+
+ReactDom.createRoot(rootElement).render(
   <StateContextProvider>
     <Router>
       <App />
     </Router>
-  </StateContextProvider>,
-  document.getElementById('root'),
+  </StateContextProvider>
 );
